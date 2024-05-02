7c3207c93f155790a2d0a316f99d8dac66968bac
MariaBertanha <113601174+MariaBertanha@users.noreply.github.com>
2024-05-02 17:27:12 -0300

    02/05

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 contadores[0].textContent = calculaTempo(tempos[0]);
 
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = cauculaTempo(tempos[i]);
+        contadores[i].textContent = cauculaTempo(tempos[i])
     }
 
 
