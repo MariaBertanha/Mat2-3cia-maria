f52349a74b24f15f2e32e8b1d8a8a6b713e03cd3
MariaBertanha <113601174+MariaBertanha@users.noreply.github.com>
2024-05-09 17:45:04 -0300

    9/05

--- main.js
+++ main.js
@@ -22,12 +22,13 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempos[0]);
-
+function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = cauculaTempo(tempos[i])
     }
-
+}
+atualizaCronometro();
+setInterval(atualizaCronometro(),1000)
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
